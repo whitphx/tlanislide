6139189314a4fe5d689cb7b1a7c4f71df5d76b52
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-12-09 14:21:10 +0900

    Update comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
 createRoot(document.getElementById("root")!).render(
-  // <StrictMode>  // TODO: StrictMode calls the render method twice, and tldraw has a bug on it; the editor is initialized twice without disposing the first one.
+  // <StrictMode>  // TODO: StrictMode calls the render method twice, and tldraw has a bug on it; the editor is initialized twice without disposing the first one. https://github.com/tldraw/tldraw/issues/5089
   <App />
   // </StrictMode>
 );
