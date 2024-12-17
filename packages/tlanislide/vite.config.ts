db23f557a2841a73e927b26382ec8cc2e8015ccd
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2024-12-17 11:41:16 +0900

    Configure Vite

--- packages/tlanislide/vite.config.ts
+++ packages/tlanislide/vite.config.ts
@@ -13,6 +13,12 @@ export default defineConfig({
     },
     rollupOptions: {
       external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   }
 })
