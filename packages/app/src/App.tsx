80b2333cffc0f54ad76885807e1e5fbe90680827
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-01-25 12:24:28 +0900

    App (#27)
    * Init packages/app
    
    * Embed <Tlanislide /> to app

---
+++ packages/app/src/App.tsx
@@ -0,0 +1,12 @@
+import { Tlanislide } from "tlanislide";
+import "tlanislide/tlanislide.css";
+
+function App() {
+  return (
+    <div style={{ position: "fixed", inset: 0 }}>
+      <Tlanislide />
+    </div>
+  );
+}
+
+export default App;
