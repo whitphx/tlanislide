f3871ec534583b24122d7c15d235f86f02b3a0f5
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-13 15:31:31 +0900

    Set up vite-plugin-font to load the Excalidraw fonts

--- packages/app/src/App.tsx
+++ packages/app/src/App.tsx
@@ -1,9 +1,27 @@
 import { Anipres } from "anipres";
+import { css, fontFamilyFallback } from "../public/XiaolaiSC-Regular.ttf";
 import "anipres/anipres.css";
 
 function App() {
   return (
-    <div style={{ position: "fixed", inset: 0 }}>
+    <div
+      style={{
+        position: "fixed",
+        inset: 0,
+      }}
+    >
+      <style>{`
+        @font-face {
+          font-family: 'Excalifont-Regular';
+          src: url('/Excalifont-Regular.woff2');
+          font-weight: normal;
+          font-style: normal;
+        }
+
+        .tl-container {
+          --tl-font-draw: Excalifont-Regular, '${css.family}', ${fontFamilyFallback}, 'tldraw_draw';
+        }
+      `}</style>
       <Anipres />
     </div>
   );
