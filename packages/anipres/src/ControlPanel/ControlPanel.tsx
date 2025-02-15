59b2f88c87fe129ba53b2285c2b40641b01f7652
Yuichiro Tachibana (Tsuchiya) <t.yic.yt@gmail.com>
2025-02-15 23:54:22 +0900

    Fix timeline scrollable layout (#108)
    * Fix the timeline style to limit the height
    
    * Add changeset

--- packages/anipres/src/ControlPanel/ControlPanel.tsx
+++ packages/anipres/src/ControlPanel/ControlPanel.tsx
@@ -127,108 +127,110 @@ export const ControlPanel = track((props: ControlPanelProps) => {
         </button>
       </div>
 
-      <Timeline
-        frameBatches={frameBatches}
-        onFrameBatchesChange={handleFrameBatchesChange}
-        onFrameChange={handleFrameChange}
-        currentStepIndex={currentStepIndex}
-        onStepSelect={(i) => {
-          const res = runStep(editor, steps, i);
-          if (res) {
-            onCurrentStepIndexChange(i);
-          }
-        }}
-        selectedFrameIds={selectedFrameShapes.map(
-          (shape) => getFrame(shape)!.id,
-        )}
-        onFrameSelect={handleFrameSelect}
-        showAttachCueFrameButton={selectedNotFrameShapes.length > 0}
-        requestAttachCueFrame={() => {
-          selectedNotFrameShapes.forEach((shape) => {
-            if (shape.type !== SlideShapeType) {
-              attachCueFrame(editor, shape.id, { type: "shapeAnimation" });
+      <div className={styles.scrollableContainer}>
+        <Timeline
+          frameBatches={frameBatches}
+          onFrameBatchesChange={handleFrameBatchesChange}
+          onFrameChange={handleFrameChange}
+          currentStepIndex={currentStepIndex}
+          onStepSelect={(i) => {
+            const res = runStep(editor, steps, i);
+            if (res) {
+              onCurrentStepIndexChange(i);
             }
-          });
-        }}
-        requestCueFrameAddAfter={(prevCueFrame) => {
-          const prevShape = getShapeByFrameId(editor, prevCueFrame.id);
-          if (prevShape == null) {
-            return;
-          }
+          }}
+          selectedFrameIds={selectedFrameShapes.map(
+            (shape) => getFrame(shape)!.id,
+          )}
+          onFrameSelect={handleFrameSelect}
+          showAttachCueFrameButton={selectedNotFrameShapes.length > 0}
+          requestAttachCueFrame={() => {
+            selectedNotFrameShapes.forEach((shape) => {
+              if (shape.type !== SlideShapeType) {
+                attachCueFrame(editor, shape.id, { type: "shapeAnimation" });
+              }
+            });
+          }}
+          requestCueFrameAddAfter={(prevCueFrame) => {
+            const prevShape = getShapeByFrameId(editor, prevCueFrame.id);
+            if (prevShape == null) {
+              return;
+            }
 
-          const newCueFrame: CueFrame = {
-            id: uniqueId(),
-            type: "cue",
-            globalIndex: steps.length + 999999, // NOTE: This will be recalculated later.
-            trackId: prevCueFrame.trackId,
-            action: {
-              type: prevCueFrame.action.type,
-              duration: 1000,
-            },
-          };
-          const newFrameBatch: FrameBatch = {
-            id: `batch-${newCueFrame.id}`,
-            globalIndex: newCueFrame.globalIndex,
-            trackId: newCueFrame.trackId,
-            data: [newCueFrame],
-          };
-          const newFrameBatches = insertOrderedTrackItem(
-            frameBatches,
-            newFrameBatch,
-            prevCueFrame.globalIndex + 1,
-          );
-          for (const batch of newFrameBatches) {
-            batch.data[0].globalIndex = batch.globalIndex;
-          }
+            const newCueFrame: CueFrame = {
+              id: uniqueId(),
+              type: "cue",
+              globalIndex: steps.length + 999999, // NOTE: This will be recalculated later.
+              trackId: prevCueFrame.trackId,
+              action: {
+                type: prevCueFrame.action.type,
+                duration: 1000,
+              },
+            };
+            const newFrameBatch: FrameBatch = {
+              id: `batch-${newCueFrame.id}`,
+              globalIndex: newCueFrame.globalIndex,
+              trackId: newCueFrame.trackId,
+              data: [newCueFrame],
+            };
+            const newFrameBatches = insertOrderedTrackItem(
+              frameBatches,
+              newFrameBatch,
+              prevCueFrame.globalIndex + 1,
+            );
+            for (const batch of newFrameBatches) {
+              batch.data[0].globalIndex = batch.globalIndex;
+            }
 
-          editor.run(
-            () => {
-              const newShapeId = createShapeId();
-              editor.createShape({
-                ...prevShape,
-                id: newShapeId,
-                x: prevShape.x + 100,
-                y: prevShape.y + 100,
-                meta: {
-                  frame: cueFrameToJsonObject(newCueFrame),
-                },
-              });
-              editor.select(newShapeId);
+            editor.run(
+              () => {
+                const newShapeId = createShapeId();
+                editor.createShape({
+                  ...prevShape,
+                  id: newShapeId,
+                  x: prevShape.x + 100,
+                  y: prevShape.y + 100,
+                  meta: {
+                    frame: cueFrameToJsonObject(newCueFrame),
+                  },
+                });
+                editor.select(newShapeId);
 
-              handleFrameBatchesChange(newFrameBatches);
-            },
-            { history: "ignore" },
-          );
-        }}
-        requestSubFrameAddAfter={(prevFrame) => {
-          const prevShape = getShapeByFrameId(editor, prevFrame.id);
-          if (prevShape == null) {
-            return;
-          }
+                handleFrameBatchesChange(newFrameBatches);
+              },
+              { history: "ignore" },
+            );
+          }}
+          requestSubFrameAddAfter={(prevFrame) => {
+            const prevShape = getShapeByFrameId(editor, prevFrame.id);
+            if (prevShape == null) {
+              return;
+            }
 
-          const newSubFrame: SubFrame = {
-            id: uniqueId(),
-            type: "sub",
-            prevFrameId: prevFrame.id,
-            action: {
-              type: "shapeAnimation",
-              duration: 1000,
-            },
-          };
+            const newSubFrame: SubFrame = {
+              id: uniqueId(),
+              type: "sub",
+              prevFrameId: prevFrame.id,
+              action: {
+                type: "shapeAnimation",
+                duration: 1000,
+              },
+            };
 
-          const newShapeId = createShapeId();
-          editor.createShape({
-            ...prevShape,
-            id: newShapeId,
-            x: prevShape.x + 100,
-            y: prevShape.y + 100,
-            meta: {
-              frame: frameToJsonObject(newSubFrame),
-            },
-          });
-          editor.select(newShapeId);
-        }}
-      />
+            const newShapeId = createShapeId();
+            editor.createShape({
+              ...prevShape,
+              id: newShapeId,
+              x: prevShape.x + 100,
+              y: prevShape.y + 100,
+              meta: {
+                frame: frameToJsonObject(newSubFrame),
+              },
+            });
+            editor.select(newShapeId);
+          }}
+        />
+      </div>
     </div>
   );
 });
